e1f8d95b3a724e8ea2c612b163a9bb20c2a69936
Wylie Thomas <wyliethomas@Wylies-MacBook-Pro.local>
2013-11-16 17:51:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreyOrangeGrey::Application.config.secret_token = '3c071fe32709f73e526f4825f4222adea25d1028d587388af3885a0acd3e34180f8d74248e905e0529d3130a5169f0756f58ffd65f55cbd289972fa877459a03'
